192c84fd8b6637caccc974e7249a8b719a5bd217
AndrewMMickel <andrew.m.mickel@gmail.com>
2020-11-13 13:32:33 -0800

    add tests and gitignore

---
+++ CalendarTests/ModelsTests/LeapYearTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calendar;
+
+namespace CalendarTests
+{
+  [TestClass]
+  public class LeapYearTests
+  {
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+        LeapYear testLeapYear = new LeapYear();
+        Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
+    }
+
+    
+  }
+}
